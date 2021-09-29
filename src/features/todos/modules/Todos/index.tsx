669e2e052966a0a9c8ccf19053769f0ce76ee917
Dean Ohashi <ohashidean@gmail.com>
2021-09-28 22:09:07 -0700

    Refactored offset pagination

--- src/features/todos/modules/Todos/index.tsx
+++ src/features/todos/modules/Todos/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useState } from 'react';
 import CreateTodoCard from 'features/todos/components/CreateTodoCard';
 import {
   MutationCompleteTodoArgs,
@@ -8,10 +8,9 @@ import {
   useResetTodoMutation,
   useCompleteTodoMutation,
   useDeleteTodoMutation,
-  useTodosLazyQuery,
+  useTodosQuery,
 } from 'generated/graphql';
 import cloneDeep from 'lodash/cloneDeep';
-import keyBy from 'lodash/keyBy';
 import TodoList from 'features/todos/components/TodoList';
 import Button from 'ui/components/Button';
 import Container from 'ui/components/Container';
@@ -21,63 +20,49 @@ const MAX_LIMIT = 5;
 
 const Todos = (): JSX.Element => {
   // Local state
-  const [todosById, setTodosById] = useState({});
+  const [todos, setTodos] = useState([]);
   const [editId, setEditId] = useState<string | null>(null);
-  const [offset, setOffset] = useState(0);
+  const [offset, setOffset] = useState(MAX_LIMIT);
   const [canPaginate, setCanPaginate] = useState(true);
 
   // GraphQL
   const [completeTodo] = useCompleteTodoMutation();
   const [deleteTodo] = useDeleteTodoMutation();
   const [resetTodo] = useResetTodoMutation();
-  const [getTodos, { data, loading }] = useTodosLazyQuery();
-  const memoizedTodos = useMemo(() => Object.values(todosById), [todosById]);
+  const { loading, error, fetchMore } = useTodosQuery({
+    variables: { limit: MAX_LIMIT, offset: 0 },
+    onCompleted: (response) => {
+      const todosFromServer = response?.todos?.todos ?? ([] as Todo[]);
 
-  // Lifecycle
-  useEffect(() => {
-    getTodos({ variables: { limit: MAX_LIMIT, offset } });
-  }, []);
-
-  useEffect(() => {
-    const todos = data?.todos?.todos;
-
-    if (Array.isArray(todos)) {
-      // Add todos and update offset
-      if (todos.length) {
-        setTodosById({ ...todosById, ...keyBy(todos, 'id') });
-      }
-
-      if (todos.length === MAX_LIMIT) {
-        setOffset((prevOffset) => prevOffset + MAX_LIMIT);
-      } else {
-        setCanPaginate(false);
-      }
-    }
-  }, [loading]);
+      setTodos([...todos, ...todosFromServer]);
+    },
+  });
 
   // Handlers
   const handleAddTodos = (todo: Todo): void => {
-    const updatedTodosById = { [todo.id]: todo, ...(todosById as Todo) };
-
-    setTodosById(updatedTodosById);
+    setTodos([todo, ...todos]);
   };
 
   const handleDeleteTodo = async (id: string): Promise<void> => {
     const variables: MutationDeleteTodoArgs = { id };
+
     await deleteTodo({ variables });
 
-    const clonedTodosById: Todo = cloneDeep(todosById as Todo);
-    delete clonedTodosById[id];
+    const clonedTodos: Todo[] = cloneDeep(todos);
+    const index = clonedTodos.findIndex((clonedTodo) => clonedTodo.id === id);
 
-    setTodosById(clonedTodosById);
+    clonedTodos.splice(index, 1);
+
+    setTodos(clonedTodos);
   };
 
-  const updateTodoById = (id: string, todo: Todo = {} as Todo): void => {
-    const clonedTodosById = cloneDeep(todosById);
+  const updateTodo = (id: string, todo: Todo = {} as Todo): void => {
+    const clonedTodos = cloneDeep(todos);
+    const index = clonedTodos.findIndex((clonedTodo) => clonedTodo.id === id);
 
-    clonedTodosById[id] = todo;
+    clonedTodos[index] = { ...todo };
 
-    setTodosById(clonedTodosById);
+    setTodos(clonedTodos);
   };
 
   const handleCompleteTodo = async (id: string): Promise<void> => {
@@ -85,15 +70,13 @@ const Todos = (): JSX.Element => {
     const response = await completeTodo({
       variables,
     });
-
     const completedTodo = response.data?.completeTodo?.todo ?? {};
 
-    updateTodoById(id, completedTodo as Todo);
+    updateTodo(id, completedTodo as Todo);
   };
 
   const handleUpdateTodo = (updatedTodo: Todo): void => {
-    updateTodoById(updatedTodo.id, updatedTodo);
-
+    updateTodo(updatedTodo.id, updatedTodo);
     setEditId(null);
   };
 
@@ -102,58 +85,63 @@ const Todos = (): JSX.Element => {
     const response = await resetTodo({
       variables,
     });
-
     const updatedTodo = response.data?.resetTodo?.todo ?? ({} as Todo);
 
-    updateTodoById(updatedTodo.id, updatedTodo as Todo);
+    updateTodo(updatedTodo.id, updatedTodo as Todo);
   };
 
   const handleSetEdit = (id: string): void => {
     setEditId(id);
   };
 
-  const fetchTodosWithOffset = async () => {
-    await getTodos({ variables: { limit: MAX_LIMIT, offset } });
+  const fetchAdditionalTodos = async () => {
+    const response = await fetchMore({
+      variables: { limit: MAX_LIMIT, offset },
+    });
+    const additionalTodos = response?.data?.todos?.todos;
+
+    setTodos([...todos, ...additionalTodos]);
+    setOffset((prevOffset) => prevOffset + MAX_LIMIT);
+
+    if (additionalTodos?.length < MAX_LIMIT) {
+      setCanPaginate(false);
+    }
   };
+
+  if (loading || error) {
+    return <Spinner />;
+  }
 
   return (
     <Container>
       <CreateTodoCard onCreate={handleAddTodos} />
-      {loading ? (
-        <Spinner />
-      ) : (
-        <>
-          <div
-            style={{
-              height: '40vh',
-              overflow: 'auto',
-              display: 'flex',
-              flexDirection: `${
-                offset > MAX_LIMIT ? 'column-reverse' : 'column'
-              }`,
-            }}
-          >
-            <TodoList
-              editId={editId}
-              todos={memoizedTodos as Todo[]}
-              onDelete={handleDeleteTodo}
-              onComplete={handleCompleteTodo}
-              onUpdate={handleUpdateTodo}
-              onSetEdit={handleSetEdit}
-              onReset={handleResetTodo}
-            />
-          </div>
-          <div style={{ marginTop: 4 }}>
-            <Button
-              disabled={!canPaginate}
-              title="See more"
-              variant={canPaginate ? 'primary' : undefined}
-              size="large"
-              onClick={fetchTodosWithOffset}
-            />
-          </div>
-        </>
-      )}
+      <div
+        style={{
+          height: '40vh',
+          overflow: 'auto',
+          display: 'flex',
+          flexDirection: `${offset > MAX_LIMIT ? 'column-reverse' : 'column'}`,
+        }}
+      >
+        <TodoList
+          editId={editId}
+          todos={todos}
+          onDelete={handleDeleteTodo}
+          onComplete={handleCompleteTodo}
+          onUpdate={handleUpdateTodo}
+          onSetEdit={handleSetEdit}
+          onReset={handleResetTodo}
+        />
+      </div>
+      <div style={{ marginTop: 4 }}>
+        <Button
+          disabled={!canPaginate}
+          title="See more"
+          variant={canPaginate ? 'primary' : undefined}
+          size="large"
+          onClick={fetchAdditionalTodos}
+        />
+      </div>
     </Container>
   );
 };
